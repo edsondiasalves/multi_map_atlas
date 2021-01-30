5d31d25d0ba3508acbba2e6c3e8d79163129db4e
= <edsondiasalves@hotmail.com>
2021-01-30 19:11:21 +0000

    Implementing initial camera position

---
+++ lib/bloc/configuration_event.dart
@@ -0,0 +1,18 @@
+part of 'configuration_bloc.dart';
+
+@immutable
+abstract class ConfigurationEvent {
+  final MapProvider provider;
+
+  ConfigurationEvent({this.provider});
+}
+
+class ChangeMapProviderStarted extends ConfigurationEvent {
+  ChangeMapProviderStarted({MapProvider provider}) : super(provider: provider);
+}
+
+class ChangeInitialPositionStarted extends ConfigurationEvent {
+  final City city;
+
+  ChangeInitialPositionStarted({this.city});
+}
